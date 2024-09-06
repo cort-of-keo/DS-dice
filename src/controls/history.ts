bfc8776f62647ba0855e4cd0c94cd660f6596b27
cort-of-keo <kolsen8@gmail.com>
2024-09-06 15:13:27 -0700

    Update history.ts
    Added Dedge to RecentRoll

--- src/controls/history.ts
+++ src/controls/history.ts
@@ -1,12 +1,13 @@
 import create from "zustand";
 import { immer } from "zustand/middleware/immer";
-import { Advantage, DiceCounts } from "./store";
+import { Advantage, DiceCounts, Dedge } from "./store";
 import { Die } from "../types/Die";
 
 export interface RecentRoll {
   counts: DiceCounts;
   bonus: number;
   advantage: Advantage;
+  dedge: Dedge;
   diceById: Record<string, Die>;
 }
 
